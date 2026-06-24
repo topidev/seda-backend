7cc66e7749263c7fd7c965aef10bc77ef2ecc491
topidev <topitzinmm@gmail.com>
2026-06-24 14:48:29 -0700

    change jwt to use body token first

--- src/auth/strategies/jwt-refresh.strategy.ts
+++ src/auth/strategies/jwt-refresh.strategy.ts
@@ -10,9 +10,9 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
     super({
       // Lee el token de la cookie en lugar del header Authorization
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => req?.cookies?.refresh_token ?? null,
         // Ahora leer del body
         (req: Request) => req?.body?.refreshToken ?? null,
+        (req: Request) => req?.cookies?.refresh_token ?? null,
       ]),
       secretOrKey: config.get<string>('JWT_REFRESH_SECRET')!,
       ignoreExpiration: false,
@@ -24,7 +24,7 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
   async validate(req: Request, payload: { sub: string; email: string; role: string }) {
     const fromCookie = req?.cookies?.refresh_token
     const fromBody = req?.body?.refreshToken
-    const refreshToken = req?.cookies?.refresh_token ?? req?.body?.refreshToken
+    const refreshToken = req?.body?.refreshToken ?? req?.cookies?.refresh_token 
 
     console.log('Token de cookie (últimos 10):', fromCookie?.slice(-10))
     console.log('Token de body (últimos 10):', fromBody?.slice(-10))
